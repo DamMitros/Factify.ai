abe9ac10ef824f73ced30be49e9c8d6db8571ac2
Natalia Owsiejko <n.owsiejko.147@studms.ug.edu.pl>
2025-11-03 11:55:56 +0100

    Added a test login button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Bubbles from "./components/Bubbles";
 import ButtonBubbles from "./components/ButtonBubbles";
 import InteractiveBubble from "./components/InteractiveBubble";
+import LoginButton from "./components/LoginButton";
 
 export default function LandingPage() {
   return (
     <div className="mainBackground">
+     <LoginButton />
      <Bubbles />
      <InteractiveBubble />
      <h1>Welcome to defacto.ai</h1>
